478d5cfc09d4f2cf713d240bf830e8f32a12b298
anisul islam <anisulislam980340@gmail.com>
2023-09-24 22:48:52 +0600

    function basic

---
+++ function.py
@@ -0,0 +1,17 @@
+def double_it(num):
+    result= num *2
+    print(result)
+    return result
+
+double_it(8)
+
+
+# sum function
+def sum(num1, num2):
+    result= num1+num2
+    return result
+
+total= sum(44, 52)
+final= double_it(total)
+print(total)
+print(final)
