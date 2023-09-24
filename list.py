42b9fde21272d05d0c9d93049f1446227416490a
anisul islam <anisulislam980340@gmail.com>
2023-09-25 02:46:50 +0600

    list

---
+++ list.py
@@ -0,0 +1,18 @@
+numbers = [45,56,42,89,87,32,89,59,45,93]
+
+print(numbers[3], numbers[-5])
+
+# array divider
+print(numbers[2:6])
+
+# with step
+print(numbers[1:7:2])
+
+# biporite deke chlote chile
+print(numbers[7:2: -1])
+
+print(numbers[4:])
+print(numbers[:5])
+
+print(numbers[:])
+print(numbers[: : -1])
--- modules.py
+++ modules.py
@@ -1,4 +1,4 @@
-from function import double_it
+from function import double_it 
 
 res = double_it(15)
 print(res)
