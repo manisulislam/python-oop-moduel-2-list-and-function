ef4d78e8790f92b27a1f6fafada77dec860ab696
anisul islam <anisulislam980340@gmail.com>
2023-09-25 01:11:30 +0600

    built in functions

---
+++ built-in-function.py
@@ -0,0 +1,6 @@
+# highest=max(98,65,25,100,25)
+highest=max([98,65,25,600,25])
+smallest= min([58,95,78,69,200])
+count= len([52, 85 ,65])
+total= sum([85, 96, 20,52])
+print(highest, smallest, count, total)
